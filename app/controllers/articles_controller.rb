375b2c940d52da31f8e77c54870715b91c8a21f1
Vincent ESKENAZI <vincent.eskenazi@gmail.com>
2020-03-30 17:10:13 +0200

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,12 +5,14 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
-    def show
-        @article = Article.find(params[:id])
-    end
-
     def new
         @article = Article.new
+    end
+
+    def show
+        @article = Article.find(params[:id])
+        @comment = Comment.new
+        @comment.article_id = @article.id
     end
 
     def create

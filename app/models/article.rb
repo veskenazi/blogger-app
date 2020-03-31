95d29349fac32d4cf3ae31463f095a87af4e41b3
Vincent ESKENAZI <vincent.eskenazi@gmail.com>
2020-03-31 17:25:03 +0200

    Added a few gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,11 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 5 }
   has_many :comments, dependent: :destroy
   has_many :taggings
   has_many :tags, through: :taggings, dependent: :destroy
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates :title, presence: true, length: { minimum: 5 }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
 
   def tag_list
     self.tags.collect do |tag|
